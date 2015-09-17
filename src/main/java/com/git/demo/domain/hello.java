aa21671ac12ace4f89f58aea5ee898cd69a78bd1
zblgh <zblvip123@163.com>
2015-09-17 12:26:43 +0800

    gitdemo init

---
+++ src/main/java/com/git/demo/domain/hello.java
@@ -0,0 +1,41 @@
+package com.git.demo.domain;
+
+/**
+ * ------------------------------------------------------------------
+ * 版权所有 Copyright (C) 2014 杭州热讯电子商务有限公司
+ *
+ *项目名称：gitdemo
+ *
+ *  包 名：com.git.demo.domain
+ *
+ * 类名称：hello
+ *
+ * 类描述：
+ *
+ * 文件名：hello.java
+ * 
+ * 创建者： 张斌龙（zbl@jojin.com.cn）
+ * 
+ * 创建时间：2015年9月17日-下午12:25:42
+ * 
+ * 描述：TODO
+ *-------------------------------------------------------------------
+ *修改人：
+ *
+ *修改时间：
+ *
+ *修改备注：
+ *-------------------------------------------------------------------
+ */
+public class hello {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
